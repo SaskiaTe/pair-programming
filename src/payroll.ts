41b02daf2e60a759020747b884523e434243a9f4
LEEEAAAAAA <lea.schuerch@gmail.com>
2025-05-23 11:05:49 +0200

    final commit

--- src/payroll.ts
+++ src/payroll.ts
@@ -23,12 +23,45 @@ export type Payslip = {
 };
 
 export function calculatePayslip(salary: Salary): Payslip {
-  // TODO: implement
   const result: Payslip = {
     salary: salary,
     deductions: new Map(),
     totalDeductions: 0.0,
     net: salary.gross,
   };
+
+  const { born, payday, gross } = salary;
+
+  const ageAtPayday = payday.getFullYear() - born.getFullYear();
+  const turned18 = new Date(born.getFullYear() + 18, 0, 1);
+  const turned17 = new Date(born.getFullYear() + 17, 0, 1);
+  const isPaydayAfter17 = payday >= turned17;
+
+ const annualGross = gross * 12;
+
+for(const [key, rate] of DEDUCTION_RATES.entries()){
+  let shouldApply = false;
+
+  if(["AHV", "IV", "EO"].includes(key)){
+    shouldApply = isPaydayAfter17;
+  }
+
+  if(["ALV", "NBU"].includes(key)){
+    shouldApply = annualGross >= 2500;
+  }
+
+  if(key === "PK"){
+    shouldApply = annualGross >= 22680;
+  }
+
+  if(shouldApply){
+    const amount = parseFloat((gross * rate / 100).toFixed(2));
+    result.deductions.set(key, amount);
+    result.totalDeductions += amount;
+  }
+}
+  result.totalDeductions = parseFloat(result.totalDeductions.toFixed(2));
+  result.net = parseFloat((gross - result.totalDeductions).toFixed(2));
+
   return result;
 }
